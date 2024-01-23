04c62d5f04a7880fa1b95594b68c3efd57931f02
alic3 <alic3dev@gmail.com>
2024-01-22 20:33:52 -0500

    Remove erronous react import

--- src/components/exercises/1/5.tsx
+++ src/components/exercises/1/5.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import { AnimatedCanvasAnimationFrame } from '@/components/exercises/AnimatedCanvas'
 import { ControlsState } from '@/components/exercises/CanvasControls'
 
